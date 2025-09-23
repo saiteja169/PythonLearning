6e237342dc4a3318ba68505bf7976d1855d0c903
saiteja169 <saitejakst7777@gmail.com>
2025-09-23 13:14:54 +0530

    added the relational operators program

---
+++ Relational operators.py
@@ -0,0 +1,40 @@
+print("Relational Operator")
+
+a = 96
+b = 66
+
+# Equal to (==)
+if (a == b):
+    print("You are not correct")
+else:
+    print("You are correct")
+
+# Not equal to (!=)
+if (a != b):
+    print("You are correct")
+else:
+    print("You are wrong")
+
+# Greater than (>)
+if (a > b):
+    print("You are correct")
+else:
+    print("You are wrong")
+
+# Less than (<)
+if (a < b):
+    print("You are correct")
+else:
+    print("You are wrong")
+
+# Less than or equal to (<=)
+if (a <= b):
+    print("You are correct")
+else:
+    print("You are wrong")
+
+# Greater than or equal to (>=)
+if (a >= b):
+    print("You are correct")
+else:
+    print("You are wrong")
