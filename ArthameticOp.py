391a8f301f067c6b6d551b453368baa8cf381110
saiteja169 <saitejakst7777@gmail.com>
2025-09-23 12:34:33 +0530

    added the arthametic operators program

---
+++ ArthameticOp.py
@@ -0,0 +1,20 @@
+a = 5
+b = 5
+result = a + b
+print("a+b",result)
+
+result = a * b
+print("a*b",result)
+
+result = a - b
+print("a-b",result)
+
+result = a ** b
+print("a**b",result)
+
+result = a % b
+print("a%b",result)
+
+result = a / b
+print("a/b",result)
+
